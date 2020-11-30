6feb1617b178068b0b0cc3181fdb9ec5437d4e37
atarta <atarta@htl-steyr.ac.at>
2020-11-30 13:12:14 +0100

    status 1

--- src/application/controller/StatusReader.java
+++ src/application/controller/StatusReader.java
@@ -6,6 +6,7 @@ import javafx.collections.ObservableList;
 
 import javax.swing.text.html.ListView;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -15,7 +16,7 @@ public class StatusReader {
     private String s;
 
 
-    public ObservableList readFile(String filename){
+    public ObservableList<Status> readFile(String filename){
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
@@ -33,6 +34,30 @@ public class StatusReader {
                 br.close();
             }
         }catch (IOException io){
+        }
+        return list;
+    }
+
+    public ObservableList<Status> readeFile(File file){
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            try {
+                br.readLine();
+                while ((s = br.readLine()) != null) {
+                    //s enthält die gesamte Zeile
+                    s = s.replace("\"", "");
+                    String[] words = s.split(";");
+                    Status a = new Status();
+                    a.StatusNummer = words[0];
+                    a.Status1 = words[1];
+                    list.add(a);
+                }
+            } finally {
+
+                br.close();
+            }
+        } catch (IOException io) {
         }
         return list;
     }
