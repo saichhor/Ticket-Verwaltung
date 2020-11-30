2234def5d19e276b6b4adde3a61dfc20a6807f38
saichhor <saichhor@htl-steyr.ac.at>
2020-11-30 13:06:49 +0100

    Änderungen: FXML-Files angepasst

--- src/application/controller/PriorityController.java
+++ src/application/controller/PriorityController.java
@@ -1,4 +1,24 @@
 package application.controller;
 
+import application.MyFXMLLoader;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+
 public class PriorityController {
+
+    public void saveButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void cancelButtonClicked(ActionEvent actionEvent) {
+    }
+
 }
--- src/application/controller/StatusController.java
+++ src/application/controller/StatusController.java
@@ -1,10 +1,12 @@
 package application.controller;
 
 import application.model.Status;
+import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import java.io.FileReader;
 import java.io.Reader;
 
 public class StatusController {
@@ -12,15 +14,18 @@ public class StatusController {
     public ListView<Status> statusListView;
 
 
-    public void initialize(){
+    public void initialize() throws Exception{
         StatusReader r = new StatusReader();
 
-        statusListView.setItems((r.readFile("stati.csv")));
+        FileReader reader = new FileReader("stati.csv");
     }
 
     public void ListViewclicked(MouseEvent mouseEvent) {
         //statusTextField.setText(statusListView.getSelectionModel().getSelectedItems().);
-//asd
+        //asd
 
+    }
+
+    public void cancelButtonClicked(ActionEvent actionEvent) {
     }
 }
