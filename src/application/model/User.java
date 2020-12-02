4662414f2fcf105e03ba399e9ec4cf1764db6ecd
atarta <atarta@htl-steyr.ac.at>
2020-12-02 11:39:06 +0100

    user bearbeitet #1

--- src/application/model/User.java
+++ src/application/model/User.java
@@ -1,4 +1,12 @@
 package application.model;
 
 public class User {
+
+    public String userTitel;
+    public String userVorname;
+    public String userNachname;
+    public String userStrasse;
+    public String userPLZ;
+    public String userStadt;
+    public String userAbteilung;
 }
